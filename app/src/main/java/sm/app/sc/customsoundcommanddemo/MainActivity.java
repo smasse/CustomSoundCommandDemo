a775aa80fdfde9e0e68fd73bb91fd311c1cf2364
Serge Masse <sergemasse1@gmail.com>
2022-01-15 19:43:21 -0500

    adding custom sound command feature, including external app demo 2022-1-15

--- app/src/main/java/sm/app/sc/customsoundcommanddemo/MainActivity.java
+++ app/src/main/java/sm/app/sc/customsoundcommanddemo/MainActivity.java
@@ -1,16 +1,21 @@
 package sm.app.sc.customsoundcommanddemo;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.android.material.snackbar.Snackbar;
 import java.util.Calendar;
 import java.util.Date;
 /**
- * receives intents containing soundCommandString, date-time, dc installation id, ...
+ * receives intents containing soundCommand String, date-time, dc installation id, ...
  * and sends intents containing results
  */
 public class MainActivity extends AppCompatActivity {
@@ -18,8 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
     private static final boolean LOG = true; //todo false in prod
 
+    //todo washere  maybe replace by ACTION_DEVICE_OWNER_CHANGED
     static final String SOUND_COMMAND_INTENT_ACTION = "sm.app.dc.intent.action.SOUND_COMMAND";
-    static final String SOUND_COMMAND_INTENT_EXTRA = "sm.app.dc.intent.extra.SOUND_COMMAND";
+    static final String SOUND_COMMAND_INTENT_EXTRA_CMD = "sm.app.dc.intent.extra.SOUND_COMMAND";
     static final String SOUND_COMMAND_INTENT_EXTRA_DATE_STRING =
             "sm.app.dc.intent.extra.SOUND_COMMAND_DATE_STRING";
     static final String SOUND_COMMAND_INTENT_EXTRA_TIME_MILLIS =
@@ -29,73 +35,57 @@ public class MainActivity extends AppCompatActivity {
     static final String SOUND_COMMAND_INTENT_EXTRA_APP_ID =
             "sm.app.dc.intent.extra.SOUND_COMMAND_APP_ID";
 
+    //todo washere  maybe replace by ACTION_DEVICE_OWNER_CHANGED
     static final String SOUND_COMMAND_INTENT_ACTION_RESULTS =
             "sm.app.dc.intent.action.SOUND_COMMAND_RESULTS";
     static final String SOUND_COMMAND_INTENT_EXTRA_RESULTS =
             "sm.app.dc.intent.extra.SOUND_COMMAND_RESULTS";
     static final String SOUND_COMMAND_INTENT_EXTRA_RESULTS_SUCCESS =
             "sm.app.dc.intent.extra.SOUND_COMMAND_RESULTS_SUCCESS";
-    static final String SOUND_COMMAND_INTENT_EXTRA_CMD_RESULTS =
-            "sm.app.dc.intent.extra.SOUND_COMMAND_CMD_RESULTS";
-    static final String SOUND_COMMAND_INTENT_EXTRA_DATE_STRING_RESULTS =
-            "sm.app.dc.intent.extra.SOUND_COMMAND_DATE_STRING_RESULTS";
-    static final String SOUND_COMMAND_INTENT_EXTRA_TIME_MILLIS_RESULTS =
-            "sm.app.dc.intent.extra.SOUND_COMMAND_TIME_MILLIS_RESULTS";
-    static final String SOUND_COMMAND_INTENT_EXTRA_INSTALLATION_ID_RESULTS =
-            "sm.app.dc.intent.extra.SOUND_COMMAND_INSTALLATION_ID_RESULTS";
-    static final String SOUND_COMMAND_INTENT_EXTRA_APP_ID_RESULTS =
-            "sm.app.dc.intent.extra.SOUND_COMMAND_APP_ID_RESULTS";
+
+    //todo washere  maybe replace by ACTION_DEVICE_OWNER_CHANGED
+    static final String SOUND_COMMAND_INTENT_ACTION_RECEPTION_NOTIF =
+            "sm.app.dc.intent.action.SOUND_COMMAND_RECEPTION_NOTIF";
 
     String soundCommandFromX = "";
+
+    SoundCommandReceiver soundCommandReceiver = null;
+    IntentFilter intentFilter = null;
+
+    TextView textViewCommand = null;
+    EditText editTextResults = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //textViewCommand
-        TextView textViewCommand = findViewById(R.id.textViewCommand);
-        //editTextResults
-        EditText editTextResults = findViewById(R.id.editTextResults);
-        //buttonSendSuccess
+        textViewCommand = findViewById(R.id.textViewCommand);
+        editTextResults = findViewById(R.id.editTextResults);
         Button buttonSendSuccess = findViewById(R.id.buttonSendSuccess);
         buttonSendSuccess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendBroadcast(buildIntentForResultsToDC(editTextResults.getText().toString(),true));
-                Snackbar.make(null,"The success results were sent to DC.",Snackbar.LENGTH_SHORT).show();
+                //Snackbar.make(null,"The success results were sent to DC.",Snackbar.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"The success results were sent to DC.",
+                        Toast.LENGTH_SHORT).show();
             }
         });
-        //buttonSendFailure
         Button buttonSendFailure = findViewById(R.id.buttonSendFailure);
         buttonSendFailure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendBroadcast(buildIntentForResultsToDC(editTextResults.getText().toString(),false));
-                Snackbar.make(null,"The failure results were sent to DC.",Snackbar.LENGTH_SHORT).show();
+                //Snackbar.make(null,"The failure results were sent to DC.",Snackbar.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"The failure results were sent to DC.",
+                        Toast.LENGTH_SHORT).show();
             }
         });
 
-        Intent intent = getIntent();
-        String action = intent.getAction();
-        if(action==null){
-            //todo show error???
-        }else {
-            if (action.equals(SOUND_COMMAND_INTENT_ACTION)) {
-                //receiving a sound command from dolphin or other external participant
-                soundCommandFromX = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA);
-                String dateString = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_DATE_STRING);
-                Long timeMillisLong = intent.getLongExtra(SOUND_COMMAND_INTENT_EXTRA_TIME_MILLIS,0L);
-                String installationId = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_INSTALLATION_ID);
-                String appId = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_APP_ID);
-
-                //show reception of sound command from DC in
-                textViewCommand.setText(soundCommandFromX);
-            }else{
-                // launch without sound command, maybe normal launch
-                // normal if action is "android.intent.action.MAIN"
-                textViewCommand.setText("The app was launched without a sound command.");
-            }
-        }
+        soundCommandReceiver = new SoundCommandReceiver();
+        soundCommandReceiver.mainActivityObject = this;
+        intentFilter.addAction(SOUND_COMMAND_INTENT_ACTION);
+        registerReceiver(soundCommandReceiver,intentFilter);
     }
 
     /**
@@ -112,17 +102,77 @@ public class MainActivity extends AppCompatActivity {
         intent.addCategory(Intent.CATEGORY_DEFAULT);
         intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_RESULTS,results);
         intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_RESULTS_SUCCESS,success);
-        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_CMD_RESULTS,soundCommandFromX);
-        //intent.setType("text/plain");
-        //date-time millis
-        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_TIME_MILLIS_RESULTS,System.currentTimeMillis());
-        //date-time string
-        Calendar cal = Calendar.getInstance();
-        Date date = cal.getTime();
-        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_DATE_STRING_RESULTS,""+date);
-        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_APP_ID_RESULTS,getClass().getName());
-        //dc installation id
-        //todo intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_INSTALLATION_ID_RESULTS, Installation.getID());
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_CMD,soundCommandFromX);
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_TIME_MILLIS,System.currentTimeMillis());
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_DATE_STRING,""+Calendar.getInstance().getTime());
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_INSTALLATION_ID,"todo");
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_APP_ID,getClass().getName());
         return intent;
+    }
+
+    /**
+     * input: soundCommandFromX
+     */
+    Intent buildIntentForReceptionNotifToDC(){
+        Intent intent = new Intent();
+        intent.setAction(SOUND_COMMAND_INTENT_ACTION_RECEPTION_NOTIF);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_CMD,soundCommandFromX);
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_TIME_MILLIS,System.currentTimeMillis());
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_DATE_STRING,""+Calendar.getInstance().getTime());
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_INSTALLATION_ID,"todo");
+        intent.putExtra(SOUND_COMMAND_INTENT_EXTRA_APP_ID,getClass().getName());
+        return intent;
+    }
+
+    protected void onDestroy() {
+        unregisterReceiver(soundCommandReceiver);
+        soundCommandReceiver.mainActivityObject = null;
+        super.onDestroy();
+    }
+
+    /**
+     * Designed to be called by the SoundCommandReceiver class, for example
+     * when receiving a potentially valid Intent.
+     *
+     * @param intent the received Intent
+     * @param context from Android when receiving an Intent.
+     */
+    void writeInCommandGui(final Intent intent, final Context context){
+        String soundCommand = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_CMD);
+        Toast.makeText(context, "Received sound command {" + soundCommand+"}",
+                Toast.LENGTH_SHORT).show();
+        soundCommandFromX = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_CMD);
+        String dateString = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_DATE_STRING);
+        long timeMillisLong = intent.getLongExtra(SOUND_COMMAND_INTENT_EXTRA_TIME_MILLIS,0L);
+        String installationId = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_INSTALLATION_ID);
+        String appId = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_APP_ID);
+        //show reception of sound command from DC in gui
+        textViewCommand.setText("Sound command received = {"+soundCommandFromX+"}"+
+                "\n\ndateString = {"+dateString+"}"+
+                "\n\ntimeMillisLong = "+timeMillisLong+
+                "\n\ninstallationId = {"+installationId+"}"+
+                "\n\nappId = {"+appId+"}"
+        );
+    }
+
+    /**
+     * Designed to be called by the SoundCommandReceiver class, for example
+     * from an anomaly in the received Intent.
+     *
+     * @param text
+     */
+    void writeInCommandGui(String text){
+        textViewCommand.setText(text);
+    }
+
+    /**
+     * Designed to be called by the SoundCommandReceiver class, for example
+     * from the bg thread for a long command execution.
+     *
+     * @param text
+     */
+    void writeInResultsGui(String text){
+        editTextResults.setText(text);
     }
 }
---
+++ app/src/main/java/sm/app/sc/customsoundcommanddemo/SoundCommandReceiver.java
@@ -0,0 +1,80 @@
+package sm.app.sc.customsoundcommanddemo;
+
+import static sm.app.sc.customsoundcommanddemo.MainActivity.SOUND_COMMAND_INTENT_ACTION;
+import static sm.app.sc.customsoundcommanddemo.MainActivity.SOUND_COMMAND_INTENT_EXTRA_CMD;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class SoundCommandReceiver extends BroadcastReceiver {
+
+    Object mainActivityObject = null;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if(SOUND_COMMAND_INTENT_ACTION.equals(action)) {//this is not necessary but safer
+            // 1. setup
+            MainActivity mainActivity = (MainActivity)mainActivityObject;
+            // 2. verify
+            String soundCommandFromX = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_CMD);
+            if(soundCommandFromX==null||soundCommandFromX.length()==0){
+                // anomaly
+                mainActivity.writeInCommandGui(
+                        "The received intent does not contain a sound command.");
+                Toast.makeText(context,"The received intent does not contain a sound command.",
+                        Toast.LENGTH_SHORT).show();
+            }else {
+                // 3. gui
+                mainActivity.writeInCommandGui(intent, context);
+                // 4. ack: notif reception: send reception notif intent to dc
+                mainActivity.sendBroadcast(mainActivity.buildIntentForReceptionNotifToDC());
+                //todo washere  see https://developer.android.com/training/snackbar/showing
+                // use CoordinatorLayout
+//                Snackbar.make(null,"The reception notification was sent to DC.",
+//                        Snackbar.LENGTH_SHORT).show();
+
+                // 5. the big work on bg thread
+                final BroadcastReceiver.PendingResult pr = goAsync();
+                Thread t = new Thread() {
+                    public void run() {
+                        processSoundCommand(intent,pr,context);
+                    }
+                };
+                t.start();
+            }
+        }
+    }
+
+    /**
+     * Must call pr.finish() at the end.
+     *
+     * @param pr BroadcastReceiver.PendingResult from goAsync()
+     */
+    private void processSoundCommand(final Intent intent,
+                                     final BroadcastReceiver.PendingResult pr,
+                                     final Context context){
+        String soundCommandFromX = intent.getStringExtra(SOUND_COMMAND_INTENT_EXTRA_CMD);
+        Toast.makeText(context,"Sound command {"+soundCommandFromX+"} is being processed...",
+                Toast.LENGTH_SHORT).show();
+
+        //todo write your code here to process the sound command
+
+
+        // the results are sent by h-user using the buttons
+        // after going to the receiver app manually (this app);
+        // this is seen by the h-user after opening this app manually
+        MainActivity mainActivity = (MainActivity)mainActivityObject;
+        mainActivity.writeInResultsGui("No additional results for sound command {"+
+                soundCommandFromX+"}.");
+        mainActivity.sendBroadcast(mainActivity.buildIntentForResultsToDC(
+                "No additional results for sound command {"+
+                soundCommandFromX+"}.",true));
+
+        //mandatory
+        pr.finish();
+    }
+
+}
